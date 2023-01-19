1cb102e8493262fad74ae074e1bc004e4be42dd0
Prasadtulasi <sivaprasadtulasi480@gmail.com>
2023-01-19 18:18:27 +0530

    updated

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
+//apiKey:"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",
+
 const { Configuration, OpenAIApi } = require("openai");
-const express = require('express');
+const express=require('express')
+const cors=require('cors')
+const json=require('body-parser')
+
 const configuration = new Configuration({
-    organization: "org-n8egb9krfGr12MqR0mevyzPD",
-    apiKey: "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",
+    organization: "org-3UJENqaG6eREJVcg9AJsSz4w",
+    apiKey:"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",
 });
 const openai = new OpenAIApi(configuration);
 
-async function callApi(){
-    
-}
-callApi();
+const app=express()
+
+app.use(json())
+app.use(cors())
 
 
-const app = express()
-const port = 3080
+const port=3080
 
-app.post('/', async(req, res) => {
+app.post('/', async(req,res)=>{
+    const {message,currentModel}=req.body;
+    console.log(message,"message")
+    console.log(currentModel)
+    console.log(currentModel)
     const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "Say this is a test",
-        max_tokens: 7,
-        temperature: 0,
+         model: `${currentModel}`,
+        prompt: `${message}`,
+        max_tokens: 100,
+        temperature: 0.5,
       });
       console.log(response.data.choices[0].text)
-      res.json({
-        data:response.data
-      })
-});
+    res.json({
+        message:response.data.choices[0].text
+    })
+    
+})
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-});
+
+app.get('/models',async(req,res)=>{
+    const response = await openai.listEngines();
+    console.log(response.data.data)
+    res.json({
+        models:response.data.data
+    })
+    console.log(response.data.data)
+})
+
+
+app.listen(port,()=>{
+    console.log(`server running on http://localhost:${port}`)
+})
